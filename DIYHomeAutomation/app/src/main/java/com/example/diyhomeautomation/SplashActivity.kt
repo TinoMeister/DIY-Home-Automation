f4f8b71c0543100f0f6da8da23be057ce111d4c9
tinomeister <tiagooliveira869@gmail.com>
2023-10-18 12:25:11 +0100

    Complete the login transaction

--- DIYHomeAutomation/app/src/main/java/com/example/diyhomeautomation/SplashActivity.kt
+++ DIYHomeAutomation/app/src/main/java/com/example/diyhomeautomation/SplashActivity.kt
@@ -1,13 +1,18 @@
 package com.example.diyhomeautomation
 
+import android.app.ActivityOptions
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.View
 import android.view.animation.AnimationUtils
 import android.widget.ImageView
 import android.widget.TextView
 import java.util.Timer
 import kotlin.concurrent.schedule
+import android.util.Pair as UtilPair
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,14 +22,23 @@ class SplashActivity : AppCompatActivity() {
         val topAnim = AnimationUtils.loadAnimation(this, R.anim.splash_top_animation)
         val bottomAnim = AnimationUtils.loadAnimation(this, R.anim.splash_bottom_animation)
 
-        findViewById<ImageView>(R.id.splash_logo_iv).animation = topAnim
-        findViewById<TextView>(R.id.splash_name_tv).animation = bottomAnim
-        findViewById<TextView>(R.id.splash_desc_tv).animation = bottomAnim
+        val logo = findViewById<ImageView>(R.id.splash_logo_iv)
+        val name = findViewById<TextView>(R.id.splash_name_tv)
+        val desc = findViewById<TextView>(R.id.splash_desc_tv)
 
-        Timer().schedule(2500) {
+        logo.animation = topAnim
+        name.animation = bottomAnim
+        desc.animation = bottomAnim
+
+        Handler(Looper.getMainLooper()).postDelayed({
             val intent = Intent(this@SplashActivity, LoginActivity::class.java)
-            startActivity(intent)
-            finish()
-        }
+
+            val options = ActivityOptions.makeSceneTransitionAnimation(this@SplashActivity,
+                UtilPair.create(logo, "logo_tn"),
+                UtilPair.create(name, "name_tn"))
+
+            startActivity(intent, options.toBundle())
+
+        }, 1000)
     }
 }
