f92895f03f8244ac453044eb26b436c5f1770fb6
Francisco Arantes <Francisco.arantes.16864@gmail.com>
2023-11-03 15:12:35 +0000

    login and register implementation with firebase

--- DIYHomeAutomation/app/src/main/java/com/example/diyhomeautomation/LoginActivity.kt
+++ DIYHomeAutomation/app/src/main/java/com/example/diyhomeautomation/LoginActivity.kt
@@ -2,20 +2,36 @@ package com.example.diyhomeautomation
 
 import android.app.ActivityOptions
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.transition.ChangeBounds
 import android.util.Pair
+import android.util.Patterns
+import android.view.View
 import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
-import android.window.OnBackInvokedDispatcher
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.textfield.TextInputLayout
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    private val emailET: EditText by lazy {
+        findViewById(R.id.login_email_til)
+    }
+    private val passwordET: EditText by lazy {
+        findViewById(R.id.login_password_til)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        auth = Firebase.auth
 
         val bounds = ChangeBounds().setDuration(1000)
         window.sharedElementEnterTransition = bounds
@@ -50,4 +66,27 @@ class LoginActivity : AppCompatActivity() {
         finishAffinity()
         super.onBackPressed()
     }
+
+    fun doLogin(v: View){
+        val email = emailET.text.toString()
+        val password = passwordET.text.toString()
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val user = auth.currentUser
+                    Toast.makeText(
+                        baseContext,
+                        "Authentication success",
+                        Toast.LENGTH_SHORT,
+                    ).show()
+                } else {
+                    Toast.makeText(
+                        baseContext,
+                        "Authentication failed.",
+                        Toast.LENGTH_SHORT,
+                    ).show()
+                }
+            }
+    }
+
 }
--- DIYHomeAutomation/app/src/main/java/com/example/diyhomeautomation/RegisterActivity.kt
+++ DIYHomeAutomation/app/src/main/java/com/example/diyhomeautomation/RegisterActivity.kt
@@ -5,16 +5,61 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Pair
+import android.util.Patterns
+import android.view.TextureView
+import android.view.View
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    private val emailET: EditText by lazy {
+        findViewById(R.id.register_email_til)
+    }
+    private val passwordET: EditText by lazy {
+        findViewById(R.id.register_password_til)
+    }
+    /*private val nameET: EditText by lazy {
+        findViewById(R.id.register_name_til)
+    }*/
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+        auth = Firebase.auth
 
         val btnLogin = findViewById<Button>(R.id.register_login_btn)
         btnLogin.setOnClickListener {
             onBackPressedDispatcher.onBackPressed()
         }
     }
+
+    fun doRegister(v: View){
+        //val name = nameET.text.toString()
+        val email = emailET.text.toString()
+        val password = passwordET.text.toString()
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val user = auth.currentUser
+                    Toast.makeText(
+                        baseContext,
+                        "Register success",
+                        Toast.LENGTH_SHORT,
+                    ).show()
+                } else {
+                    Toast.makeText(
+                        baseContext,
+                        "Register failed.",
+                        Toast.LENGTH_SHORT,
+                    ).show()
+                }
+            }
+    }
+
 }
