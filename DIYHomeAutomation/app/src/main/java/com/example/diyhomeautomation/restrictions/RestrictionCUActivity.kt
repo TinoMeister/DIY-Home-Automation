67fcc6aa0719aaa2379ec7a574b88bec5b0c1fbf
tinomeister <tiagooliveira869@gmail.com>
2023-11-15 19:03:51 +0000

    Add Restrictions Design

---
+++ DIYHomeAutomation/app/src/main/java/com/example/diyhomeautomation/restrictions/RestrictionCUActivity.kt
@@ -0,0 +1,12 @@
+package com.example.diyhomeautomation.restrictions
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.diyhomeautomation.R
+
+class RestrictionCUActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_restriction_cu)
+    }
+}
