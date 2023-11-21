13d06b417f4de9f41f6cb008a7e866915dc54442
Francisco Arantes <Francisco.arantes.16864@gmail.com>
2023-11-21 15:49:48 +0000

    update api methods

--- DIYHomeAutomation/app/src/main/java/com/example/diyhomeautomation/api/DeviceApi.kt
+++ DIYHomeAutomation/app/src/main/java/com/example/diyhomeautomation/api/DeviceApi.kt
@@ -1,4 +1,30 @@
 package com.example.diyhomeautomation.api
 
+import android.devicelock.DeviceId
+import com.example.diyhomeautomation.models.Device
+import com.example.diyhomeautomation.models.User
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.DELETE
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Path
+import retrofit2.http.Query
+
 interface DeviceApi {
+    @GET("/device")
+    fun getAllDevices(): Call<List<Device>>
+
+    @GET("/device/{roomId}")
+    fun getDevice(@Path("roomId") roomId: Int): Call<Device>
+
+    @POST("/device")
+    fun postDevice(@Body value: Device): Call<Device>
+
+    @PUT("/device/{id}")
+    fun putDevice(@Path("id") id: Int, @Body value: Device): Call<Device>
+
+    @DELETE("/device/{id}")
+    fun deleteDevice(@Path("id") id: Int): Call<Any>
 }
--- DIYHomeAutomation/app/src/main/java/com/example/diyhomeautomation/api/HistoryApi.kt
+++ DIYHomeAutomation/app/src/main/java/com/example/diyhomeautomation/api/HistoryApi.kt
@@ -1,4 +1,10 @@
 package com.example.diyhomeautomation.api
 
+import com.example.diyhomeautomation.models.History
+import retrofit2.Call
+import retrofit2.http.GET
+
 interface HistoryApi {
+    @GET("/history")
+    fun getAllHistories(): Call<List<History>>
 }
